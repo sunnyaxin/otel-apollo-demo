e8eb5dd0eeca8fa55a4c433e2a1089d4f2820995
Yaxin <yaxin.wang@rea-group.com>
2025-11-15 10:34:17 +0800

    add manual trace instrumentation

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -4,8 +4,14 @@ import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { GraphQLInstrumentation } from "@opentelemetry/instrumentation-graphql";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
+import {resourceFromAttributes} from "@opentelemetry/resources";
+import {ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION} from "@opentelemetry/semantic-conventions";
 
 const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: "otel-apollo-server",
+    [ATTR_SERVICE_VERSION]: "1.0",
+  }),
   traceExporter: new OTLPTraceExporter({
     url: "http://otelcol:4318/v1/traces",
   }),
@@ -20,7 +26,7 @@ const sdk = new NodeSDK({
   instrumentations: [
     // 1.自动集成常见 Node.js 库（如 HTTP、Express、MySQL 等）的 OpenTelemetry 追踪，无需手动为每个库配置
     // 2.可以看到部分库的trace和metrics（如 HTTP、Express 等库的 instrumentation 本身支持 metrics 采集）
-    getNodeAutoInstrumentations(),
+    // getNodeAutoInstrumentations(),
 
     // 1.专门为 GraphQL 服务提供的追踪集成，只针对 GraphQL 查询和解析过程进行追踪。
     // 2.只能看到 traces，看不到 metrics
