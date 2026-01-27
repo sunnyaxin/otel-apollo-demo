e42b3a5d998a35f0be2ec604ba0414e2e2ce5070
sunnyaxin <sunnyyaxin@gmail.com>
2026-01-27 21:23:04 +0800

    [processor] filter - span.

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,13 +1,18 @@
 import { Tracer } from "@opentelemetry/api";
 import { logger } from "./logger";
+import { randomInt } from "crypto";
 
 export const resolvers = (tracer: Tracer) => ({
   Query: {
     hello: (): string => {
       tracer.startActiveSpan("hello-span-query", (it) => {
+        const number = randomInt(0, 2);
         logger.info("inside span");
+        logger.info(`generated number: ${number}`);
         it.setAttribute("useremail", "pii@example.com");
         it.setAttribute("password", "1password");
+        it.setAttribute("randomnumber", number);
+        it.addEvent("randomnumber-generated", { value: number });
         it.end();
       });
       logger.info("outside span");
